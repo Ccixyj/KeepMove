ca56a0d72067f71ed2065eef4ae1b8ed0e2ef097
徐宇健 <751762476@qq.com>
2018-12-31 23:07:02 +0800

    add es6 bable loader

--- JS/webpack/study/src/index.js
+++ JS/webpack/study/src/index.js
@@ -9,16 +9,43 @@ import "bootstrap/dist/css/bootstrap.css"
 
 function component() {
     var element = document.createElement('div');
-  
+
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
 
-  $(function(){
-      $("li:odd").css("background-color","pink")
-      $("li:even").css("background-color","lightBlue")
-  })
+    return element;
+}
+
+document.body.appendChild(component());
+
+
+$(function () {
+    $("li:odd").css("background-color", "pink")
+    $("li:even").css("background-color", "lightBlue")
+
+    class Animal {
+        static method(){
+            console.log("call static metod");
+            return "static method"
+        }
+
+        
+        // constructor(name) {
+        //     this.name = name
+        // }
+        
+        constructor({name="123", age=123}) {
+            this.name = name
+            this.age = age
+        }
+    }
+    Animal.foo  = 123
+
+    const a = new Animal({
+        name:"vivi"
+    })
+    console.log(a);
+    console.log(` ${Animal.method} call value is ${Animal.method()}` );
+    console.log(`static value foo is ${Animal.foo}` );
+})
+
--- JS/webpack/study/webpack.config.js
+++ JS/webpack/study/webpack.config.js
@@ -66,6 +66,19 @@ module.exports = {
         use: [
           'file-loader'
         ]
+      },
+      {
+        test:/\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+        
+          }
+        }
+
       }
     ]
   }
