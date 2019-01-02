85bedf22474bf1b30243bc5b75fe4808cddafde0
徐宇健 <751762476@qq.com>
2019-01-02 21:43:43 +0800

    vue cli with mintui

---
+++ JS/Vue/mintui/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <mt-button type="default" size="small">default</mt-button>
+    <mt-button type="default" size="large">default</mt-button>
+    <mt-button type="primary" size="normal">primary</mt-button>
+    <mt-button type="danger" disabled>danger</mt-button>
+    <mt-button type="danger" icon="more">danger</mt-button>
+    <mt-button type="danger" icon="back">danger</mt-button>
+    <mt-button type="danger" plain>danger</mt-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "home",
+  components: {}
+};
+</script>
+
+<style scoped>
+</style>
+
