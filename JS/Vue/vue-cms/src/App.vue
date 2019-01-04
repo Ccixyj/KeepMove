12e5ffa77becaec4710dc796e52319eaf85623b5
xuyujian <15152278073@139.com>
2019-01-04 17:03:50 +0800

    bottom

--- JS/Vue/vue-cms/src/App.vue
+++ JS/Vue/vue-cms/src/App.vue
@@ -1,29 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app" class="app-container">
+    <mt-header fixed title="固定在顶部"></mt-header>
+
+    <!-- content -->
+    
+    <!-- //tab bar -->
+    <nav class="mui-bar mui-bar-tab">
+			<router-link class="mui-tab-item mui-active" to="/home">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/member">
+				<span class="mui-icon mui-icon-email"></span>
+				<span class="mui-tab-label">会员</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/cart">
+				<span class="mui-icon mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">购物车</span>
+			</router-link>
+			<router-link class="mui-tab-item" to="/search">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">搜索</span>
+			</router-link>
+		</nav>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import Vue from "vue";
+import { Header } from "mint-ui";
+import { Tabbar, TabItem } from "mint-ui";
+import { TabContainer, TabContainerItem } from "mint-ui";
+
+Vue.component(Header.name, Header);
+Vue.component(Tabbar.name, Tabbar);
+Vue.component(TabItem.name, TabItem);
+Vue.component(TabContainer.name, TabContainer);
+Vue.component(TabContainerItem.name, TabContainerItem);
+
+import { Cell } from "mint-ui";
+Vue.component(Cell.name, Cell);
+import { Badge } from "mint-ui";
+Vue.component(Badge.name, Badge);
+
+export default {
+  created: function() {
+    console.log("app created");
   }
+};
+</script>
+
+<style lang="scss">
+.app-container {
+  padding-top: 40px;
 }
 </style>
--- JS/Vue/vue-cms/src/main.js
+++ JS/Vue/vue-cms/src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//mint ui css
+import 'mint-ui/lib/style.css'
+//mui css 注意顺序
+import '../lib/mui/css/mui.min.css'
+import '../lib/mui/css/icons-extra.css'
 
 Vue.config.productionTip = false
 
@@ -10,3 +15,5 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+console.log("init ok");
