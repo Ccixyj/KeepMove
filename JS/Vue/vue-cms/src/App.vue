a5a32cc97d254ff19eb06dece9e39c2def6c1a30
徐宇健 <751762476@qq.com>
2019-01-20 22:53:14 +0800

    home -> vant

--- JS/Vue/vue-cms/src/App.vue
+++ JS/Vue/vue-cms/src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app" class="app-container">
-    <!-- <mt-header fixed title="这是app">
-      <a @click.prevent="goBack"  slot="left" v-show="showBack">
-        <mt-button icon="back"></mt-button>
-      </a>
-    </mt-header>-->
     <van-nav-bar
       title="this app"
       :left-text="navBackTxt"
@@ -15,29 +10,9 @@
     <transition name="app-content">
       <router-view></router-view>
     </transition>
-    <!-- //tab bar -->
-    <!-- <nav class="mui-bar mui-bar-tab">
-      <router-link class="mui-tab-item" to="/home">
-        <span class="mui-icon mui-icon-home"></span>
-        <span class="mui-tab-label">首页</span>
-      </router-link>
-      <router-link class="mui-tab-item" to="/member">
-        <span class="mui-icon mui-icon-email"></span>
-        <span class="mui-tab-label">会员</span>
-      </router-link>
-      <router-link class="mui-tab-item" to="/cart">
-        <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
-          <span class="mui-badge">9</span>
-        </span>
-        <span class="mui-tab-label">购物车</span>
-      </router-link>
-      <router-link class="mui-tab-item" to="/search">
-        <span class="mui-icon mui-icon-search"></span>
-        <span class="mui-tab-label">搜索</span>
-      </router-link>
-    </nav>-->
+
     <van-tabbar v-model="active">
-      <van-tabbar-item icon="home-o"  to="/home">标签</van-tabbar-item>
+      <van-tabbar-item icon="home-o" to="/home">标签</van-tabbar-item>
       <van-tabbar-item icon="manager-o" to="/member">会员</van-tabbar-item>
       <van-tabbar-item icon="cart-o" info="5" to="/cart">购物车</van-tabbar-item>
       <van-tabbar-item icon="search" to="/search">搜索</van-tabbar-item>
@@ -47,10 +22,6 @@
 
 <script>
 import Vue from "vue";
-import { Header, Button } from "mint-ui";
-
-Vue.component(Header.name, Header);
-Vue.component(Button.name, Button);
 
 import { NavBar } from "vant";
 import { Tabbar, TabbarItem } from "vant";
@@ -58,6 +29,11 @@ Vue.use(NavBar);
 Vue.use(Tabbar).use(TabbarItem);
 
 export default {
+  data() {
+    return {
+      active: 0
+    };
+  },
   computed: {
     showBack: function() {
       return !["/home", "/member", "/cart", "/search"].includes(
--- JS/Vue/vue-cms/src/view/tabbar/HomeContainer.vue
+++ JS/Vue/vue-cms/src/view/tabbar/HomeContainer.vue
@@ -1,61 +1,73 @@
 <template>
-<!--  -->
+  <!--  -->
   <div class="home-container">
-    <mt-swipe :auto="4000">
+    <!-- <mt-swipe :auto="4000">
       <mt-swipe-item v-for="(item , index) in items" :key="index + item.url">
         <img :src="item.image" alt srcset>
       </mt-swipe-item>
-    </mt-swipe>
+    </mt-swipe>-->
+    <van-swipe :autoplay="3000" indicator-color="white">
+      <van-swipe-item v-for="(item ) in items" :key="item.url" >
+        <img v-lazy="item.image" >
+      </van-swipe-item>
+    </van-swipe>
 
-    
-    <ul class="mui-table-view mui-grid-view mui-grid-9">
-      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-        <router-link to="/home/news">
-          <img src="../../assets/images/menu1.png">
-          <div class="mui-media-body">新闻资讯</div>
-        </router-link>
-      </li>
-      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-        <a href="#">
-          <img src="../../assets/images/menu2.png">
+    <div class="center-grid">
+      <van-row type="flex" justify="space-around">
+        <van-col span="6">
+          <router-link to="/home/news">
+            <img src="../../assets/images/menu1.png">
+            <div class="col-text">新闻资讯</div>
+          </router-link>
+        </van-col>
+        <van-col span="6">
+          <a href="#">
+            <img src="../../assets/images/menu2.png">
 
-          <div class="mui-media-body">图片专区</div>
-        </a>
-      </li>
-      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-        <a href="#">
-          <img src="../../assets/images/menu3.png">
-          <div class="mui-media-body">数码购买</div>
-        </a>
-      </li>
-      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-        <a href="#">
-          <img src="../../assets/images/menu4.png">
-          <div class="mui-media-body">留言反馈</div>
-        </a>
-      </li>
-      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-        <a href="#">
-          <img src="../../assets/images/menu5.png">
-          <div class="mui-media-body">视屏专区</div>
-        </a>
-      </li>
-      <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
-        <a href="#">
-          <img src="../../assets/images/menu6.png">
-          <div class="mui-media-body">联系我们</div>
-        </a>
-      </li>
-    </ul>
+            <div class="col-text">图片专区</div>
+          </a>
+        </van-col>
+        <van-col span="6">
+          <a href="#">
+            <img src="../../assets/images/menu3.png">
+            <div class="col-text">数码购买</div>
+          </a>
+        </van-col>
+        <van-col span="6">
+          <a href="#">
+            <img src="../../assets/images/menu4.png">
+            <div class="col-text">留言反馈</div>
+          </a>
+        </van-col>
+      </van-row>
+
+      <van-row>
+        <van-col span="6">
+          <a href="#">
+            <img src="../../assets/images/menu5.png">
+            <div class="col-text">视屏专区</div>
+          </a>
+        </van-col>
+        <van-col span="6">
+          <a href="#">
+            <img src="../../assets/images/menu6.png">
+            <div class="col-text">联系我们</div>
+          </a>
+        </van-col>
+      </van-row>
+    </div>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
-import { Swipe, SwipeItem } from "mint-ui";
 
-Vue.component(Swipe.name, Swipe);
-Vue.component(SwipeItem.name, SwipeItem);
+import { Swipe, SwipeItem } from "vant";
+Vue.use(Swipe).use(SwipeItem);
+import { Row, Col } from "vant";
+Vue.use(Row).use(Col);
+import { Lazyload } from 'vant';
+Vue.use(Lazyload);
 export default {
   data() {
     return {
@@ -73,20 +85,33 @@ export default {
 
 <style lang="scss" scoped>
 .home-container {
-  .mint-swipe {
+  .van-swipe {
     height: 200px;
 
     img {
       width: 100%;
-      height: 100%;
+      height: 200px;
+      display: block;
+      pointer-events: none;
     }
   }
 
-  .mui-grid-view {
-    .mui-table-view-cell {
-      img {
-        width: 70%;
-        height: 70%;
+  .center-grid {
+    padding-top: 16px;
+    .van-row {
+      padding: 8px 0;
+
+      .van-col {
+        text-align: center;
+        font-size: 14px;
+
+        img {
+          width: 60px;
+          height: 60px;
+        }
+        .col-text {
+          color: #333;
+        }
       }
     }
   }
