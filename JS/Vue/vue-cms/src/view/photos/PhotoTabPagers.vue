7959a8db0cad57af67c23739a9aded1174df60ff
徐宇健 <751762476@qq.com>
2019-02-02 21:28:52 +0800

    add imgs

--- JS/Vue/vue-cms/src/view/photos/PhotoTabPagers.vue
+++ JS/Vue/vue-cms/src/view/photos/PhotoTabPagers.vue
@@ -1,36 +1,90 @@
 <template>
   <div id="home-photos">
-    <van-tabs v-model="active" swipeable animated  ellipsis=false>
-      <van-tab v-for="(title , index) in cate" :title="title" :key="title">内容 {{ index }}</van-tab>
+    <van-tabs v-model="active" swipeable animated :ellipsis="false" @change="getImgs">
+      <van-tab v-for="(data ) in cate" :title="data.name" :key="data.id">
+        <ul>
+          <li v-for="(item) in allImgs[data.id]" :key="item.cid" >
+            <!-- :src="item.turl" -->
+            <img v-lazy="item.turl"  alt>
+          </li>
+        </ul>
+      </van-tab>
     </van-tabs>
   </div>
 </template>
 
 <script>
-import { Tab, Tabs } from "vant";
+import { Tab, Tabs, Lazyload } from "vant";
 import Vue from "vue";
-Vue.use(Tab).use(Tabs);
+Vue.use(Tab)
+  .use(Tabs)
+  .use(Lazyload);
 export default {
   data() {
     return {
-      active:0,
-      cate: []
+      active: 0,
+      cate: [],
+      allImgs: {}
     };
   },
   methods: {
-    getCates(){
-      this.$api.get("/photos/category",null,r =>{
-          this.cate = r.data
-      },e => {
-        this.$toast.fail("加载失败!")
-      })
+    getCates() {
+      this.$api.get(
+        "/photos/category",
+        null,
+        r => {
+          this.cate = r.data;
+          this.getImgs(this.active)
+        },
+        e => {
+          this.$toast.fail("加载失败!");
+        }
+      );
+    },
+
+    getImgs(index, title) {
+      console.log("chane index" + index);
+      let cate = this.cate[index];
+      if (!this.allImgs[cate.id] || !(this.allImgs[cate.id].length <= 0)) {
+        this.$api.get(
+          `/photos/images/${cate.id}`,
+          null,
+          r => {
+            this.$nextTick(function(){
+              Vue.set( this.allImgs ,cate.id , r.data)
+              //  this.allImgs[cate.id] = r.data;
+            })
+          },
+          e => {
+            this.$toast.fail("加载失败!");
+          }
+        );
+      }
     }
   },
   created() {
-    this.getCates()
+    this.getCates();
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  watch: {
+    // active: function loadImghs(newVal, oldVal) {
+    //   console.log(`old ${oldVal} new ${newVal}`);
+    //   let id = this.cate[newVal].id;
+    //   if (!this.allImgs[id]) {
+    //     this.getImgs(newVal);
+    //   }
+    // }
   }
 };
 </script>
 
-<style>
+<style scoped lang="scss">
+#home-photos {
+  img {
+    height: 100%;
+    width: 100%;
+  }
+}
 </style>
